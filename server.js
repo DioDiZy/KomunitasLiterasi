0fe033af0a9007597b01f374fa82f4ac62879e45
DioDiZy <dioblonteng1212@gmail.com>
2025-04-25 01:38:25 +0700

    ubah deskripsi user

--- server.js
+++ server.js
@@ -40,12 +40,44 @@ const names = [
   "Rafif Syahputra Pasaribu",
   "Yola Hardini Girsang",
 ];
+const deskripsiAnak = [
+  "Anak 1",
+  "Anak 2",
+  "Anak 3",
+  "Anak 4",
+  "Anak 5",
+  "Anak 6",
+  "Anak 7",
+  "Anak 8",
+  "Anak 9",
+  "Anak 10",
+  "Anak 11",
+  "Anak 12",
+  "Anak 13",
+  "Anak 14",
+  "Anak 15",
+  "Anak 16",
+  "Anak 17",
+  "Anak 18",
+  "Anak 19",
+  "Anak 20",
+  "Anak 21",
+  "Anak 22",
+  "Anak 23",
+  "Anak 24",
+  "Anak 25",
+  "Anak 26",
+  "Anak 27",
+  "Anak 28",
+  "Anak 29",
+  "Anak 30",
+];
 // Dummy data for members
 const members = Array.from({ length: 30 }, (_, i) => ({
   id: i + 1,
   name: ` ${names[i]}`,
   key: `kunci${i + 1}`,
-  description: `Ini adalah deskripsi dari Anggota ${i + 1}.`,
+  description: `Ini adalah deskripsi dari Anggota ${deskripsiAnak[i]}`,
 }));
 
 // Endpoint to get all member names (no keys)
