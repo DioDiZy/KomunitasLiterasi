e62034a323f66804aab7ec395bc45bd715402153
DioDiZy <dioblonteng1212@gmail.com>
2025-04-25 01:15:43 +0700

    ubah nama 'anggota' jadi nama anak komunitas

--- server.js
+++ server.js
@@ -8,11 +8,42 @@ const PORT = 3000;
 
 app.use(cors());
 app.use(bodyParser.json());
-
+const names = [
+  "Dio Syahputra",
+  "Muhammad Roekhan",
+  "Livia Salsabila",
+  "Gabriella F. Pandiangan",
+  "Stevyn Angely Hutabarat",
+  "Dwi Alya Artika",
+  "Salsabila Nanda Lestari",
+  "Lutviya Setiyaningsih",
+  "Panji Akbar Dewantara",
+  "Teguh Rahmat Syahputra",
+  "Fatir Gibran",
+  "Lawrencius C.K Panjaitan",
+  "Ataka Putu Samsuri K.",
+  "Nadine Ananda Syahfitri",
+  "Puja Satria Tawakal",
+  "Raysa Salsabila",
+  "Stefani Siahaan",
+  "Farhat Huda",
+  "Nopri Anita Libertiana S",
+  "Ibrahim Prasaja Putra",
+  "Avril Chrysalis Lilihata",
+  "Davina Kezia Sianturi",
+  "Musaddad Amin",
+  "Mohammad Haikal Nur Rafidana",
+  "Annisa Nur Amelia",
+  "Aulia Isnaeni Azkatunnisa",
+  "Aliff Akbar Pelawi",
+  "Kevin Binsar Sinaga",
+  "Rafif Syahputra Pasaribu",
+  "Yola Hardini Girsang",
+];
 // Dummy data for members
 const members = Array.from({ length: 30 }, (_, i) => ({
   id: i + 1,
-  name: `Anggota ${i + 1}`,
+  name: ` ${names[i]}`,
   key: `kunci${i + 1}`,
   description: `Ini adalah deskripsi dari Anggota ${i + 1}.`,
 }));
@@ -35,7 +66,7 @@ app.post("/api/unlock", (req, res) => {
   if (member.key === key) {
     return res.json({ unlocked: true, description: member.description });
   } else {
-    return res.status(401).json({ unlocked: false, error: "Invalid key" });
+    return res.status(401).json({ unlocked: false, error: "Passwordnya salah, coba lagi" });
   }
 });
 
