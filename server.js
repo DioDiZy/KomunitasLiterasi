362f7266d9e8a56c6e4ed869ef071ca806c4d695
DioDiZy <dioblonteng1212@gmail.com>
2025-04-25 02:47:16 +0700

    yuh bisa yuh

--- server.js
+++ server.js
@@ -78,7 +78,7 @@ const members = Array.from({ length: 30 }, (_, i) => ({
   name: ` ${names[i]}`,
   key: `kunci${i + 1}`,
   description: `Ini adalah deskripsi dari Anggota ${deskripsiAnak[i]}`,
-  // photo: `public/images/${i + 1}.jpg`,
+  photo: `/images/${i + 1}.jpg`,
 }));
 
 // Endpoint to get all member names (no keys)
